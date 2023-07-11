50ebf0ab4f62788dcf228e27db233ea03684489a
Aathiq Ahamed <aathiq.20210466@iit.ac.lk>
2023-07-11 09:33:34 +0530

    answers added upto 6

--- mongodb/answers.js
+++ mongodb/answers.js
@@ -4,19 +4,58 @@ db.createCollection("employee");
 db.createCollection("department");
 db.createCollection("faction");
 db.createCollection("sub");
+
 printjson(
-  "Count of people are working at HMS are ",
+  "Number of people are working at HMS are ",
   db.employee.find().count()
 );
 printjson(
-  "Count of people are working at ENG are ",
+  "Number of people are working at ENG are ",
   db.employee.find({ department_id: "2" }).count()
 );
 printjson(
-  "Count of people are working at ENG and working pn API sbu ",
+  "Number of people are working at ENG and working pn API sbu ",
   db.employee
     .find({
-      $and: [{ department_id: "2" }, { sbu_id: "2" }],
+      $and: [{ department_id: "2" }, { sbu_id: "3" }],
     })
     .count()
 );
+printjson(
+  "Working department of Sandarenu is ",
+  db.employee.findOne({ first_name: "Sandarenu" }).department_id
+);
+
+printjson(
+  "Number of people are working at ENG and working pn API sbu ",
+  db.employee
+    .find({
+      $and: [{ department_id: "2" }, { sbu_id: "3" }],
+    })
+    .count()
+);
+printjson(
+  "Number of people are working at ENG and working pn APIGO sbu ",
+  db.employee
+    .find({
+      $and: [{ department_id: "2" }, { sbu_id: "5" }],
+    })
+    .count()
+);
+printjson(
+  "Count of people are working at IMPL and working pn API sbu ",
+  db.employee
+    .find({
+      $and: [{ department_id: "3" }, { sbu_id: "3" }],
+    })
+    .count()
+);
+// printjson(
+//   "Sbu list which has more than 15 employees",
+//   db.employee
+//     .find({
+//       $and: [{ department_id: "3" }, { sbu_id: "3" }],
+//     })
+//     .count()
+// );
+
